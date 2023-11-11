233d148679b7adc02e20f5d11a21ca65bc534d4e
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2023-11-11 15:07:54 +0200

    Time: 146 ms (45.66%), Space: 98.9 MB (94.94%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    std::vector<int> dailyTemperatures(std::vector<int>& temperatures) {
-        int sz = static_cast<int>(temperatures.size());
-        std::vector<int> ans(sz);
-        std::stack<std::pair<int, int>> st;  // Stack of pairs (temperature, index)
-        
+    vector<int> dailyTemperatures(vector<int> &temperatures) {
+        int sz = (int) temperatures.size();
+        vector<int> ans(sz);
+        stack<int> st;
         for (int i = sz - 1; i >= 0; i--) {
-            while (!st.empty() && st.top().first <= temperatures[i]) {
+            while (!st.empty() && temperatures[st.top()] <= temperatures[i]) {
                 st.pop();
             }
-            
-            ans[i] = st.empty() ? 0 : st.top().second - i;
-            st.push({temperatures[i], i});
+            ans[i] = st.empty() ? 0 : st.top() - i;
+            st.push(i);
         }
-        
         return ans;
     }
 };
-
-
-
-
-
