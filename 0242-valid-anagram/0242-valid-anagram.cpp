eb65f3982b89250da41565f8de08798666c6939f
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2023-11-15 01:07:50 +0200

    Time: 8 ms (62.89%), Space: 7.6 MB (97.67%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        vector<int> freq(26);
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+
+        for (auto &i: freq) {
+            if (i) return false;
+        }
+
+        return true;
+    }
+};
