3e8beae6d3f5486a495f24deed6cb1d804771e5d
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2024-02-16 17:27:39 +0200

    Time: 186 ms (39.86%), Space: 67.8 MB (35.34%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -1,29 +1,27 @@
 class Solution {
 
 private:
-    static bool sortByValue(pair<int, int> &a, pair<int, int> &b) {
-        return a.second < b.second;
-    }
 
 public:
     int findLeastNumOfUniqueInts(vector<int> &arr, int k) {
         // Count frequency of each element
-        unordered_map<int, int> freq;
+        map<int, int> freq;
         for (int i: arr) freq[i]++;
 
         // Sort by value (frequency)
-        vector<pair<int, int>> unique;
-        for (auto &it: freq) unique.emplace_back(it);
-        sort(unique.begin(), unique.end(), sortByValue);
+        vector<int> unique;
+        for (auto it = freq.rbegin(); it != freq.rend(); it++) {
+            unique.emplace_back(it->second);
+        }
+        sort(unique.begin(), unique.end());
 
         // number of unique elements
         int ans = int(unique.size());
 
         // Remove elements with lowest frequency
         for (auto &i: unique) {
-            if (k < i.second) break;
-            k -= i.second;
-            ans--;
+            if (k < i) break;
+            k -= i, ans--;
         }
 
         return ans;
