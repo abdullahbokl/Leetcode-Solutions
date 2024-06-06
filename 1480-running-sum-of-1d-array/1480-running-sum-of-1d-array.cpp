e1d695802b346c703573f42db40ff02ec44a48c7
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2024-06-06 19:53:35 +0300

    Time: 0 ms (100.00%), Space: 10.6 MB (15.89%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,11 +1,12 @@
+
 class Solution {
 public:
     vector<int> runningSum(vector<int> &nums) {
         int sum = 0;
-        for_each(nums.begin(), nums.end(), [&](int &num) {
+        for (auto &num: nums) {
             sum += num;
             num = sum;
-        });
+        }
 
         return nums;
     }
