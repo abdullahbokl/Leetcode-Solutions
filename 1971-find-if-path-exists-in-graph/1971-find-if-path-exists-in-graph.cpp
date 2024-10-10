094ac17436c8c17512ac634041dde557605d2855
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2024-10-10 09:44:50 +0300

    Time: 2069 ms (18.85%), Space: 378.3 MB (19.85%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
 
     vector<vector<int>> graph = vector<vector<int>>();
+    vector<bool> visited =  vector<bool>();
 
-    bool dfs(int source, int destination, vector<bool> &visited) {
+    bool dfs(int source, int destination) {
         if (source == destination) {
             return true;
         }
         visited[source] = true;
-        for (int i = 0; i < graph[source].size(); i++) {
-            if (!visited[graph[source][i]]) {
-                if (dfs(graph[source][i], destination, visited)) {
+        for (int i: graph[source]) {
+            if (!visited[i]) {
+                if (dfs(i, destination)) {
                     return true;
                 }
             }
@@ -21,12 +22,12 @@ public:
 
     bool validPath(int n, vector<vector<int>> &edges, int source, int destination) {
         graph = vector<vector<int>>(n);
+        visited = vector<bool>(n, false);
         for (auto edge: edges) {
             graph[edge[0]].push_back(edge[1]);
             graph[edge[1]].push_back(edge[0]);
         }
-        vector<bool> visited = vector<bool>(n, false);
-        return dfs(source, destination, visited);
+        return dfs(source, destination);
 
     }
 };
