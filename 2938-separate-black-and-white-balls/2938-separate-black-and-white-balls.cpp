d7be71d9bb91974cc0355938e037bcd3420ed8d7
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2024-10-15 16:16:34 +0300

    Time: 31 ms (73.47%), Space: 15.6 MB (76.42%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        int lastWhite = 0;
-        for (int i = s.size() - 1; i >= 0; --i) {
-            if (s[i] == '0') {
-                lastWhite = i;
-                break;
-            };
-        }
-
         long long ans = 0;
+        int ones = 0;
 
-        for (int i = lastWhite - 1; i >= 0; --i) {
-            if (s[i] == '1') ans += lastWhite - i, lastWhite--;
+        for (const char c: s) {
+            c == '1' ? ++ones : ans += ones;
         }
 
         return ans;
     }
-
 };
