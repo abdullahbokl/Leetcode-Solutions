c2a7c7d263f577f2b3941c9cc85ce71c33d92b38
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2024-10-25 00:22:34 +0300

    Time: 2 ms (95.12%), Space: 14 MB (99.74%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,30 +1,17 @@
 class Solution {
 private:
-    const int sz = 2e4 + 5;
-    int dp[20005];
-    map<int, int> freq;
-
-    int _solve(int n) {
-        if (n < 0) return 0;
-
-        if (dp[n] != -1) return dp[n];
-
-        int take = -1;
-        if (freq[n] != 0) {
-            take = n * freq[n] + _solve(n - 2);
-        }
-        int leave = _solve(n - 1);
-
-
-        return dp[n] = max(take, leave);
-    }
-
+    int arr[20004];
 public:
     int deleteAndEarn(vector<int> &nums) {
-        memset(dp, -1, sizeof(dp));
-        freq.clear();
-        int mx = -1;
-        for (auto &num: nums) freq[num]++, mx = max(mx, num);
-        return _solve(mx);
+        fill(arr, arr + 20004, 0);
+        for (int &num: nums) arr[num] += num;
+
+        arr[1] = max(arr[0], arr[1]);
+
+        for (int i = 2; i < 20004; ++i) {
+            arr[i] = max(arr[i] + arr[i-2], arr[i-1]);
+        }
+
+        return arr[20003];
     }
 };
