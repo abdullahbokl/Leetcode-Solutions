0a948cddb0764f1f0a7b1a6a660cab56237a8477
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2024-10-24 21:11:27 +0300

    Time: 0 ms (100.00%), Space: 7.5 MB (67.00%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,23 +1,15 @@
 class Solution {
 private:
     int dp[50];
-
-    int _steps(int n) {
-        if (n < 0) return 0;
-        if (n == 0) return 1;
-
-        if (dp[n] != -1) return dp[n];
-
-        dp[n] = _steps(n - 1) + _steps(n - 2);
-        return dp[n];
-    }
-
 public:
     int climbStairs(int n) {
+        dp[0] = 1;
 
-        fill(dp, dp + 50, -1);
+        for (int i = 1; i <= n; ++i) {
+            if (i - 2 < 0) dp[i] = dp[i - 1];
+            else dp[i] = dp[i - 1] + dp[i - 2];
+        }
 
-        return _steps(n);
-
+        return dp[n];
     }
 };
