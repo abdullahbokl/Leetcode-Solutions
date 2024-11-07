58ff915d22212a0cb964b7b53b45c7760fc87807
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2024-11-07 19:29:36 +0200

    Time: 0 ms (100.00%), Space: 7.5 MB (95.89%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -5,10 +5,13 @@ public:
         if (s.size() != goal.size()) return false;
         if (s == goal) return true;
 
+        int cnt = 0;
+
         for (int i = 0; i < s.size(); i++) {
             if (s == goal) return true;
-            s.push_back(s[0]);
-            s.erase(0, 1);
+            s.push_back(s[0]), s.erase(0, 1);
+            i = 0, cnt++;
+            if (cnt == s.size() * 2) break;
         }
         return false;
 
