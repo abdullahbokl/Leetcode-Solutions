685089ccca5d7185f02cfdb972b211aceb95a304
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2024-11-09 09:54:48 +0200

    Time: 0 ms (100.00%), Space: 8.4 MB (82.17%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.cpp
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
+        int i = 0, j = 0;
+        n--;
         long long ans = x;
 
-        while (--n) {
-            ans += 1;
-            ans |= x;
+        while (i < 64 and j < 64) {
+            if (x & (1LL << i)) {
+                i++;
+                continue;
+            }
+            long long currentN = (n & (1LL << j));
+            if (currentN) {
+                ans = ans | (1LL << i);
+            }
+            j++;
+            i++;
         }
-
         return ans;
     }
 };
