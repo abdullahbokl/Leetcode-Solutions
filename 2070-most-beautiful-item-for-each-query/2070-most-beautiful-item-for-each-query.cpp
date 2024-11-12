17aa475ffbb979a76fa45674bb23c08477299707
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2024-11-12 23:39:04 +0200

    Time: 75 ms (33.33%), Space: 103.2 MB (23.67%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
@@ -1,21 +1,55 @@
 class Solution {
+
+private:
+    int binarySearch(vector<pair<int, int>> &v, int target) {
+        int l = 0, r = v.size() - 1, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (v[mid].first <= target) {
+                if (ans == -1) ans = mid;
+                else if (v[mid].second > v[ans].second) {
+                    ans = mid;
+                }
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+
 public:
-    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+    vector<int> maximumBeauty(vector<vector<int>> &items, vector<int> &queries) {
+
+        map<int, int> mp;
+        int n = (int) items.size();
+
+        for (int i = 0; i < n; i++) {
+            if (mp[items[i][0]] < items[i][1]) {
+                mp[items[i][0]] = items[i][1];
+            }
+        }
+
+        vector<pair<int, int>> v;
+        for (auto &i: mp) v.emplace_back(i);
+
+        for (int i = 1; i < v.size(); i++) {
+            if (v[i].second < v[i - 1].second) {
+                v[i].second = v[i - 1].second;
+            }
+        }
+
         vector<int> ans;
-        vector<int> prices;
-        vector<int> maxBeautySoFar(items.size() + 1);
+        for (int i = 0; i < queries.size(); i++) {
+            int q = queries[i];
+            int idx = binarySearch(v, q);
 
-        sort(items.begin(), items.end());
-
-        for (const vector<int>& item : items)
-            prices.push_back(item[0]);
-
-        for (int i = 0; i < items.size(); ++i)
-            maxBeautySoFar[i + 1] = max(maxBeautySoFar[i], items[i][1]);
-
-        for (const int query : queries) {
-            int i = upper_bound(prices.begin(), prices.end(), query) - prices.begin();
-            ans.push_back(maxBeautySoFar[i]);
+            if (idx == -1) {
+                ans.push_back(0);
+            } else {
+                ans.push_back(v[idx].second);
+            }
         }
 
         return ans;
