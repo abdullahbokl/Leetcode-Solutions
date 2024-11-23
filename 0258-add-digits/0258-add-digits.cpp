7533573edb4b517896afade23d17c781a219fe78
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2024-11-23 20:02:31 +0200

    Time: 0 ms (100.00%), Space: 8.4 MB (65.75%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,15 +1,6 @@
 class Solution {
-public:
-    int addDigits(int num) {
-        int sum = num;
-        while ((double) sum / 10.0 >= 1.0) {
-            num = sum;
-            sum = 0;
-            while (num) {
-                sum += num % 10;
-                num /= 10;
-            }
-        }
-        return sum;
-    }
+ public:
+  int addDigits(int num) {
+    return 1 + (num - 1) % 9;
+  }
 };
