fd76e17a4577798e02340fd520385326c357352c
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2024-12-04 12:08:14 +0200

    Time: 0 ms (100.00%), Space: 17.1 MB (13.81%) - LeetHub

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int i = 0;  // Index for str2
+
+        for (const char c : str1) {
+            if (c == str2[i] || ('a' + ((c - 'a' + 1) % 26)) == str2[i]) {
+                if (++i == str2.length()) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
