ba4c116dc50830dd635e538db17e6ac8dcc86cc5
Abdullah Khaled Elbokl <113001767+abdullahbokl@users.noreply.github.com>
2024-12-06 13:29:28 +0200

    Time: 52 ms (88.17%), Space: 117.6 MB (82.66%) - LeetHub

--- 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,29 +1,19 @@
 class Solution {
-    // binary seach to check if the number is banned
-    bool isBanned(vector<int> &banned, int n) {
-        int l = 0, r = banned.size() - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (banned[mid] == n) return true;
-            if (banned[mid] < n) l = mid + 1;
-            else r = mid - 1;
-        }
-        return false;
-    }
-
 public:
     int maxCount(vector<int> &banned, int n, int maxSum) {
         sort(banned.begin(), banned.end());
-        int r = 1, sum = 0, cnt = 0;
+        int l = 0, r = 1, sum = 0, cnt = 0, sz = static_cast<int>(banned.size());
         while (r <= n and sum + r <= maxSum) {
-            if (isBanned(banned, r)) {
-                r++;
+            while (l < sz and banned[l] < r) l++;
+
+            if (l < sz and banned[l] == r) {
+                l++, r++;
                 continue;
             }
+
             sum += r++;
             cnt++;
         }
-
         return cnt;
     }
 };
